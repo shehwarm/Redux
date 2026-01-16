f598549d015ca8f16fe859bc3c0ad2dc49e02013
Shehwar Maryam <shehwarm93@gmail.com>
2026-01-16 18:43:32 +0500

    feat: update store configuration to use todoReducer

--- redux-class/src/app/store.js
+++ redux-class/src/app/store.js
@@ -1,5 +1,7 @@
 import { confgureStore } from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+
 
 export const store = confgureStore({
-    reducer: {},
+    reducer: todoReducer,
 });
